cf8f16e4168c3c3df5677164eb9c9b4ded07aa11
7sug <baikal728@gmail.com>
2021-10-22 19:44:46 +0300

    Initial commit

---
+++ src/main/java/sma.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class sma {
+    public static void main(String args[]) {
+        List<Integer> answer = smaWorker.smaFunc();
+        System.out.println(answer);
+    }
+}
---
+++ src/main/java/smaWorker.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class smaWorker {
+    public static List<Integer> smaFunc(){
+        List<Integer> sma = new ArrayList<Integer>();
+        //int[] data = {2, 5, 7, 8, 3, 0, 10, 11, 15};
+        List<Integer> data = DataGenerator.randomizer();
+        int arrayLen = data.size();
+        int sum = 0;
+        int count = 0;
+        int count2 = 3;
+        for (int i = 3; i < arrayLen; i++)
+        {
+            for (int j = count; j < count2; j++)
+                sum += data.get(j);
+            count += 1;
+            count2 += 1;
+            int mid = sum / 3;
+            sum = 0;
+            sma.add(mid);
+        }
+        return sma;
+    }
+}
